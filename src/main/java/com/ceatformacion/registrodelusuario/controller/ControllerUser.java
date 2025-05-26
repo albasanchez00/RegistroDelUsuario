dcca32742098a88291286508ce23ab43612cd451
albasanchez00 <albasanchezolmedo00@gmail.com>
2025-05-26 20:55:49 +0200

    Commit ejercicio corregido

--- src/main/java/com/ceatformacion/registrodelusuario/controller/ControllerUser.java
+++ src/main/java/com/ceatformacion/registrodelusuario/controller/ControllerUser.java
@@ -5,12 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 @Controller
 public class ControllerUser {
@@ -28,11 +29,74 @@ public class ControllerUser {
 
     @PostMapping("/guardar")
     public String guardarUsuario(@ModelAttribute Usuario usuario, Model model){
+        //Asegurarse que la fecha está correctamente configurada
         usuario.setFechaRegistro(String.valueOf(LocalDate.now()));
+        usuario.asignarIdUsuario();
+        //Agregar el usuario a la lista
         listaUsuarios.add(usuario);
+
+        //Mostrar por consola para depurar
         System.out.println(usuario);
-        return "crud";
+
+        return "redirect:/crud";
     }
+
+    @GetMapping("/crud")
+    public String mostrarUsuarios(Model model){
+        //Pasar la lista de usuarios a la vista
+        model.addAttribute("usuarios", listaUsuarios);
+        return "crud"; //Nombre de la vista para mostrar los usuarios
+    }
+
+    @GetMapping("/editar/{idUsuario}")
+    public String modificarUsuario(@PathVariable int idUsuario, Model model){
+        //Usuario donde almacenará el objeto Usuario encontrado en el Iterator, para pasarlo
+        //a la vista -> modificar.html
+        Usuario usuarioE = null;
+        //Iterator para recorrer el ArrayList y ubicar el ID que se pasa como parámetro
+        Iterator<Usuario> itUsuario = listaUsuarios.iterator();
+        while (itUsuario.hasNext()){
+            Usuario user = itUsuario.next();
+            if(idUsuario == user.getIdUsuario()){
+                usuarioE = user;
+                break;
+            }
+        }
+        if(listaUsuarios!=null){
+            model.addAttribute("usuario", usuarioE);
+            return "modificar";
+        }else {
+            //Si la lista no se encuentra o la lista esta vacía.
+            return "redirect:/crud";
+        }
+    }
+
+    @PostMapping("/modificar")
+    public String guardarModificacion(Model model, Usuario usuarioActualizado){
+        //Recorremos el ArrayList buscando el id del usuario que llega, y después remplazo todos sus datos
+        for(int i=0; i<listaUsuarios.size(); i++){
+            if(listaUsuarios.get(i).getIdUsuario()==usuarioActualizado.getIdUsuario()){
+
+                listaUsuarios.set(i, usuarioActualizado); //Se reemplazan todos sus datos
+                break; //nos salimos
+            }
+        }
+        //Redirigimos al Crud
+        return "redirect:/crud";
+    }
+
+    @GetMapping("/eliminar/{idUsuario}")
+    public String eliminarUsuario(@PathVariable int idUsuario){
+        for(int i=0; i<listaUsuarios.size(); i++){
+            if(listaUsuarios.get(i).getIdUsuario()==idUsuario){
+                listaUsuarios.remove(i);
+                break;
+            }
+        }
+        return "redirect:/crud";
+    }
+
+
 
 
 }
--- src/main/java/com/ceatformacion/registrodelusuario/modell/Usuario.java
+++ src/main/java/com/ceatformacion/registrodelusuario/modell/Usuario.java
@@ -7,17 +7,16 @@ public class Usuario {
     private String email;
     private String userName;
     private String password;
-    static int id=1;
-    private int idUsario=0;
+    static int id=0;
+    private int idUsuario =0;
     private LocalDate fechaRegistro;
 
 
     //Método Constructor
     public Usuario(){
-        this.idUsario = id++;
     }
-    public Usuario(String nombreApellido, String email, String userName, String password, String fechaRegistro) {
-        this();
+    public Usuario(int idUsuario, String nombreApellido, String email, String userName, String password, String fechaRegistro) {
+        this.idUsuario = idUsuario;
         this.nombreApellido = nombreApellido;
         this.userName = userName;
         this.password = password;
@@ -26,54 +25,62 @@ public class Usuario {
     }
 
     //Métodos Getter y Setter
-    public String getNombreApellido() {
-        return nombreApellido;
+    //Metodo asignarID temporal porque cuando usemos BBDD este id se autogenera.
+    public void asignarIdUsuario() {
+        this.idUsuario =++id;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
     }
 
     public void setNombreApellido(String nombreApellido) {
         this.nombreApellido = nombreApellido;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getUserName() {
-        return userName;
     }
 
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getIdUsario() {
-        return idUsario;
-    }
-
-    public LocalDate getFechaRegistro() {
-        return fechaRegistro;
     }
 
     public void setFechaRegistro(String fechaRegistro) {
         this.fechaRegistro = LocalDate.parse(fechaRegistro);
     }
 
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+    public String getNombreApellido() {
+        return nombreApellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getFechaRegistro() {
+        return String.valueOf(this.fechaRegistro);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
     //Método toString
     @Override
     public String toString() {
-        return "User ID("+idUsario+")" +
+        return "User ID("+ idUsuario +")" +
                 "User name='" + userName + '\'' +
                 "Nombre y apellido:'" + nombreApellido + '\'' +
                 "Email:" + email + '\'' +
